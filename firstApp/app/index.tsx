ce944bbebc89612cf94aa80fab18039371c7875d
Iván Carrascosa Barragán <ivan.carrascosa@iesnervion.es>
2025-09-27 18:07:04 +0200

    boton con alerta

--- firstApp/app/index.tsx
+++ firstApp/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Alert, Pressable, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +10,17 @@ export default function Index() {
       }}
     >
       <Text>Hello world</Text>
+      <TouchableOpacity
+        onPress={() => Alert.alert("Hello world!")}
+        style={{
+          backgroundColor: "#007BFF",
+          padding: 10,
+          borderRadius: 5,
+          marginTop: 10,
+        }}
+        >
+        <Text style={{ color: "#FFFFFF" }}>Click me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
